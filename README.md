e4445755146f919fc47c6bdd6ec08382d9528aad
josefl19 <jose.fl_@hotmail.com>
2021-11-25 19:29:38 -0600

    Avance de introduccion

---
+++ README.md
@@ -0,0 +1,4 @@
+# Introducción al análisis de datos con Python
+
+Repositorio creado para el curso de introducción al análisis de datos con Python de CodigoFacilito
+Curso tomado dentro del Bootcamp de Ciencia de Datos.
---
+++ prueba.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+def resta(x, y):
+    return x-y
+
+def multiplicacion(m, n):
+    return m*n
+
+def multiplicacion(m, n):
+    return m*n
+
+get_ipython().run_line_magic('save', '-a prueba.py 4-5')
+def multiplicacion(m, n):
+    return m*n
